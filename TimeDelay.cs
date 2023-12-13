3502b08c68f11838a5822330f16eb27a940ae2fb
Spandana <36356836+spany14@users.noreply.github.com>
2023-12-13 13:04:22 +0100

    Delay Functions by few seconds
    script that waits a few seconds before calling a function without using coroutine

---
+++ TimeDelay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDelay : MonoBehaviour
+{
+    [SerializeField]
+    float interval = 5f;
+
+    float timeHolder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeHolder = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeHolder += Time.deltaTime;
+        
+        while (timeHolder >= interval) 
+        {
+            
+            DebugText();
+            timeHolder -= interval;
+        }
+        
+    }
+
+
+    void DebugText()
+    {
+        Debug.Log(" 5 seconds passed  " + System.DateTime.Now);
+    }
+}
